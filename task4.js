538ed274a582d269d49ec7bf9411f8703ae0e524
Habib Muhammad Rizki <habibboboho@gmail.com>
2024-12-01 23:08:04 +0700

    Add task 4 no 2

--- task4.js
+++ task4.js
@@ -1,3 +1,4 @@
+// 1
 const cekPalindrome = (name) => {
   const reversedName = name.split("").reverse().join("");
   if (name === reversedName) {
@@ -9,3 +10,10 @@ const cekPalindrome = (name) => {
 
 cekPalindrome("malam");
 cekPalindrome("pagi");
+
+//2
+const reverseWords = (kalimat) => {
+  return kalimat.split(" ").reverse().join(" ");
+};
+
+console.log(reverseWords("Saya Belajar Javascript"));
